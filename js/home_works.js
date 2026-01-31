96814670a429e3ed516a3e1d02a3fba681de6375
Stariy-131 <antonkuzmin2006men@gmail.com>
2026-01-31 21:53:01 +0600

    create moving for cub
    Добавлена функция движения куба по кругу.

--- js/home_works.js
+++ js/home_works.js
@@ -2,7 +2,7 @@ const gmailInput = document.querySelector("#gmail_input");
 const gmailBtn = document.querySelector("#gmail_button");
 const gmailResult = document.querySelector("#gmail_result");
 
-const regExp = /^\w+@gmail\.com$/;
+const regExp = /^\w+@gmail\.com$|^\w+@mail\.ru$|^\w+@yandex\.ru$|^\w+@email\.com$/;
 
 gmailBtn.onclick = () => {
     if (regExp.test(gmailInput.value)) {
@@ -15,19 +15,35 @@ gmailBtn.onclick = () => {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+    const box = document.querySelector(".parent_block");
     const block = document.querySelector(".child_block");
 
+
+
+    const offsetWidth = box.clientWidth - block.clientWidth;
+    const offsetHeight = box.clientHeight - block.clientHeight;
+
     let positionX = 0;
+    let positionY = 0;
 
     function moveBlock() {
-        positionX += 0.5;
-        block.style.transform = `translateX(${positionX}px)`;
+        block.style.transform = `translate(${positionX}px, ${positionY}px)`;
 
-        if (positionX < 449) {
-            requestAnimationFrame(moveBlock);
+        if (positionX < offsetWidth && positionY === 0) { 
+            positionX += 3;
+        } 
+        else if (positionX >= offsetWidth && positionY < offsetHeight) { 
+            positionY += 3;
+        } 
+        else if (positionX > 0 && positionY >= offsetHeight) {
+            positionX -= 3;
+        } 
+        else if (positionX <= 0 && positionY > 0) {
+            positionY -= 3;
         }
+        requestAnimationFrame(moveBlock);
+        
     }
-
     moveBlock();
     
 });
